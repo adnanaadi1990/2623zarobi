aac42a5d3390f9cab80d73cf1223371fef15944e
Adnan khan <adnan.yousafzai@internatioinaltextile.com>
2017-02-17 10:22:34 +0500

    done

--- Dashboard_Project/DashboardProject/DashboardProject/Modules/Master/BOMApproval.aspx.cs
+++ Dashboard_Project/DashboardProject/DashboardProject/Modules/Master/BOMApproval.aspx.cs
@@ -895,7 +895,7 @@ namespace DashboardProject.Modules.Master
                     UserName = reader["user_name"].ToString();
                     UserEmail = reader["user_email"].ToString(); //ViewState["SessionUser"].ToString();
                     EmailSubject = "BOM Approval Request – Form ID # " + lblMaxTransactionID.Text.ToString() + "";
-                    EmailBody = "Dear Mr " + "" + UserName.ToString() + ",<br> <br>   " + ViewState["SessionUser"].ToString() + ",<br> <br> BOM Approval Request has been issued against Form ID #  " + lblMaxTransactionID.Text.ToString() +
+                    EmailBody = "Dear Mr " + "" + UserName.ToString() + ",<br> <br>   " + ViewState["SessionUser"].ToString() + " has been update BOM Approval Request against Form ID #  " + lblMaxTransactionID.Text.ToString() +
                      "The form can be reviewed at the following URL within ITL Network:<br><a href =" + url.ToString() + ">" + url.ToString() + "</a> <br> <br>" +
                      "To access the form outside ITL network, please use the following URL:<br><a href =" + urlMobile.ToString() + ">" + urlMobile.ToString() + "</a> <br> <br> " +
                      "This is an auto-generated email from IS Dashboard,<br> you do not need to reply to this message." +
